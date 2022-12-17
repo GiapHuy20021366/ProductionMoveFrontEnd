ef14d451c36070f517a8361b675dac1cc6a5eb20
quynhnt <trangquynh17@gmail.com>
2022-12-17 22:17:20 +0700

    merge

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,6 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Login from '../components/containers/Login';
 import Home from '../components/containers/Home';
-import Dashboard from '../components/containers/Dashboard';
 import '../styles/App.scss';
 import { paths } from '../untils/constant';
 import { userIsAuthenticated, userIsNotAuthenticated } from '../components/hoc/authentication'
@@ -36,7 +35,6 @@ const App = () => {
           <Route exact path={paths.HOME} component={Home} />
           <Route path={paths.LOGIN} component={userIsNotAuthenticated(Login)} />
           <Route path={paths.SYSTEM} component={userIsAuthenticated(System)} />
-          <Route path={paths.DASHBOARD} component={userIsAuthenticated(Dashboard)} />
           
         </Switch>
       </BrowserRouter>
