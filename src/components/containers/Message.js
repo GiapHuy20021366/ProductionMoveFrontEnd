914c40e5ce0e7400b7a73217700f8d501b659b73
minhminhhn <anilinform23@gmail.com>
2022-12-30 16:25:25 +0700

    message

--- src/components/containers/Message.js
+++ src/components/containers/Message.js
@@ -8,37 +8,58 @@ import "../../vendor/jquery/jquery.min";
 import "../../vendor/bootstrap/js/bootstrap.bundle.min";
 import "../../styles/sb-admin-2.min.css";
 import "../../styles/font.css";
+import { useHistory, withRouter } from "react-router";
+import { paths } from "../../untils/constant";
 import { useSelector } from "react-redux";
 
 const Message = () => {
   const messages = useSelector((state) => state.message);
-  const subLang = useSelector(state => state.lang.AdminAccounts)
+  const subLang = useSelector((state) => state.lang.AdminAccounts);
+  const account = useSelector((state) => state.user.account);
+
+  const history = useHistory();
 
   console.log(messages);
   console.log(subLang);
 
-  const getRole = (roleId) => {
-      switch (roleId) {
-          case 1:
-              return subLang.admin
-          case 2:
-              return subLang.factory
-          case 3:
-              return subLang.agency
-          case 4:
-              return subLang.maintain_center
-          default:
-              return subLang.unknown
+  const onClickProducts = () => {
+    switch (account.role) {
+        case 1:
+          return history.push(paths.ADMIN_PRODUCTS)
+        case 2:
+          return history.push(paths.FACTORY_PRODUCTS)
+        case 3:
+          return history.push(paths.AGENCY_PRODUCTS)
+        case 4:
+          return history.push(paths.MAINTENANCE_PRODUCTS)
+        default:
+          return "#"
       }
-  }
+    
+}
+
+  const getRole = (roleId) => {
+    switch (roleId) {
+      case 1:
+        return subLang.admin;
+      case 2:
+        return subLang.factory;
+      case 3:
+        return subLang.agency;
+      case 4:
+        return subLang.maintain_center;
+      default:
+        return subLang.unknown;
+    }
+  };
   length = messages.list.length;
   console.log(length);
   const typeMessage = (message) => {
     if (message.content.type == "EXPORT_CONFIRM_NOTIFICATION") {
-      return `đã xác nhận ${message.content.exports.length} sản phẩm được xuất đi`;
+      return `đã xác nhận ${message.content.exports.length} sản phẩm được xuất đi.`;
     }
     if (message.content.type == "EXPORT_NOTIFICATION") {
-      return `đã xuất ${message.content.exports.length} sản phẩm cần xác nhận`;
+      return `đã xuất ${message.content.exports.length} sản phẩm cần xác nhận.`;
     }
   };
 
@@ -68,11 +89,19 @@ const Message = () => {
             <>
               <div className="dropdown-item d-flex align-items-center">
                 <div>
-                  <div className="small text-gray-500">{message.date.substring(0, 10)}</div>
+                  <div className="small text-gray-500">
+                    {message.date.substring(0, 10)}
+                  </div>
                   <span className="font-weight-bold">
-                    {getRole(message.content.from.role)} {" "} 
-                    {message.content.from.name} {" "}
-                    {typeMessage(message)} {" "}
+                    {getRole(message.content.from.role)}{" "}
+                    {message.content.from.name} {typeMessage(message)}{" "}
+                    
+                            <a href="#" onClick={() => {
+                                onClickProducts()
+                            }}>
+                            Xem chi tiết
+                            </a>
+
                     
                   </span>
                 </div>
